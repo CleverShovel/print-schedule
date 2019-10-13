45e0e4dcdbe5fcc55462015b30371fc2488ceb6f
CleverShovel <quest201114@yandex.ru>
2019-10-13 22:00:39 +0400

    image creating from schedule

--- db/__init__.py
+++
@@ -1,9 +0,0 @@
-from peewee import *
-from configparser import ConfigParser
-
-config = ConfigParser()
-config.read('./config.ini')
-db_config = config['DATABASE']
-
-db = MySQLDatabase(db_config['db_name'], user=db_config['user'], password=db_config['password'],
-                   host=db_config['host'], port=int(db_config['port']))
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-from db import db
+from jinja2 import Environment, FileSystemLoader
+import imgkit
+import json
+import sys
+
+lessons = json.load(open(sys.argv[1], "r"))
+
+templateLoader = FileSystemLoader(searchpath="./")
+templateEnv = Environment(loader=templateLoader)
+
+template = templateEnv.get_template('schedule_wo_bootstrap.html')
+res = template.render(lessons=lessons)
+
+# css = './css/bootstrap.min.css'
+imgkit.from_string(res, 'out.jpg', options={
+   'format': 'png',
+    # 'crop-h': '300',
+    'crop-w': '310',
+    'crop-x': '3',
+    'crop-y': '3'
+})
