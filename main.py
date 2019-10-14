d47ebfc68d5eb6095462d215ab99122ef68e4e2e
CleverShovel <quest201114@yandex.ru>
2019-10-14 22:14:15 +0400

    template bug solved

--- main.py
+++ main.py
@@ -11,6 +11,22 @@ lessons = json.load(input_file)
 templateLoader = FileSystemLoader(searchpath="./")
 templateEnv = Environment(loader=templateLoader)
 
+def max_length_of_groups(groups):
+    return max(map(lambda x : max(map(lambda y: y['subgroup'], x.list)), groups))
+
+def add_empty_subgroups(lessons):
+    max_subgroup = max(int(x['subgroup']) for x in lessons)
+    if max_subgroup == 0:
+        return lessons
+    subgroups_lessons = {int(x['subgroup']): x for x in lessons}
+    for i in range(1, max_subgroup + 1):
+        if i not in subgroups_lessons:
+            subgroups_lessons[i] = None
+    return [y for (x,y) in sorted(subgroups_lessons.items())]
+
+templateEnv.filters['add_empty_subgroups'] = add_empty_subgroups
+templateEnv.filters['max_length_of_groups'] = max_length_of_groups
+
 template = templateEnv.get_template('schedule_wo_bootstrap.html')
 res = template.render(lessons=lessons)
 
@@ -18,7 +34,7 @@ res = template.render(lessons=lessons)
 imgkit.from_string(res, output_file, options={
    'format': 'png',
     # 'crop-h': '300',
-    'crop-w': '310',
+    'crop-w': '610',
     'crop-x': '3',
     'crop-y': '3'
 })
